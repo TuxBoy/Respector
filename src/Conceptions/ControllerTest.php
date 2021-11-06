91210785ab9caf79ffcdae115b35afc35f0da522
TuxBoy <benoit.mallo@gmail.com>
2021-11-06 16:16:35 +0100

    feat: Move abstract class test in src

---
+++ src/Conceptions/ControllerTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Respector\Conceptions;
+
+use Generator;
+use Respector\Analyze;
+use Respector\ParsedFile;
+
+abstract class ControllerTest extends BaseConceptionTest
+{
+    /**
+     * @dataProvider getAnalyzedFiles
+     */
+    public function testIfControllerHasNewRouteSyntax(ParsedFile $parsedFile): void
+    {
+        $class = $parsedFile->getClass();
+        $this->assertStringEndsWith('Controller', $class->getName());
+        $this->assertIsFinal($class);
+    }
+
+    /**
+     * @dataProvider getAnalyzedFiles
+     */
+    public function testIfControllerUsedAttributeRoute(ParsedFile $parsedFile): void
+    {
+        $class = $parsedFile->getClass();
+        $this->assertHasAttributeRoute($class);
+    }
+
+    public function getAnalyzedFiles(): Generator
+    {
+        return Analyze::create('composer.json')
+            ->inNamespace('Infrastructure\\Symfony\\Controller')
+            ->getDataProvider();
+    }
+}
